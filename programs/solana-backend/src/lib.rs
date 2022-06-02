b54ee00ae6a1751e244b0b353b45c88653d8f352
Ryan Carter <whiteapple8222@gmail.com>
2022-06-02 01:26:28 +0000

    update for transfer

--- programs/solana-backend/src/lib.rs
+++ programs/solana-backend/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{TokenAccount};
-use anchor_lang::system_program::{transfer, Transfer};
+use anchor_spl::token::{self, TokenAccount, Token, Transfer};
+use anchor_lang::system_program;
+use anchor_lang::system_program::{transfer};
 declare_id!("AVD6temvivgz6p1S9xaSNmnLnfPdsQfwNVXyVEGCSr44");
 
 #[program]
@@ -11,12 +12,28 @@ pub mod solana_backend {
         {
             let cpi_ctx = CpiContext::new(
                 ctx.accounts.system_program.to_account_info(),
-                Transfer {
+                system_program::Transfer {
                     from: ctx.accounts.from.to_account_info(),
                     to: ctx.accounts.to.to_account_info(),
                 },
             );
             transfer(cpi_ctx, amount)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn transfer_out(ctx: Context<TransferOut>) -> Result<()> {
+        {
+            let cpi_ctx = CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.from.to_account_info(),
+                    to: ctx.accounts.to.to_account_info(),
+                    authority: ctx.accounts.owner.to_account_info(),
+                },
+            );
+            token::transfer(cpi_ctx, 1)?;
         }
 
         Ok(())
@@ -29,8 +46,19 @@ pub struct TransferIn<'info> {
     from: Account<'info, TokenAccount>,
     #[account(mut)]
     to: Account<'info, TokenAccount>,
-    /// CHECK: not now
     mint: AccountInfo<'info>,
     owner: Signer<'info>,
-    system_program: Program<'info, System>
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>
+}
+
+#[derive(Accounts)]
+pub struct TransferOut<'info> {
+    #[account(mut)]
+    from: Account<'info, TokenAccount>,
+    #[account(mut)]
+    to: Account<'info, TokenAccount>,
+    mint: AccountInfo<'info>,
+    owner: Signer<'info>,
+    token_program: Program<'info, Token>,
 }
